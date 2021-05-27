13b6afc00aeaade6da5cc5c4ce9e145a21fbd37b
maria4Q <78930278+maria4Q@users.noreply.github.com>
2021-05-27 13:38:51 -0600

    Add files via upload

---
+++ Mensaje_Inicial.java
@@ -0,0 +1,27 @@
+package scl.edu.gt;
+
+public class Mensaje_Inicial {
+
+   public static void Mensaje() {
+	   
+	  
+	   System.out.println(" $$$$__$$$$  $$$$$$$  $$$$$$$  $$  $$$$$$$");  
+	   System.out.println(" $$  00  $$  $$   $$  $$   $$  $$  $$   $$");
+	   System.out.println(" $$      $$  $$___$$  $$$$$    $$  $$___$$");
+	   System.out.println(" $$      $$  $$   $$  $$   $$  $$  $$   $$");
+	  
+	   
+   }
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
